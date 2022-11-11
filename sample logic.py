1ac6686aa3ba70c40e1e32a083794c1cc5b98651
Jenver <denialsys@gmail.com>
2022-11-11 21:09:44 +0800

    added the logic for testing

--- sample logic.py
+++ sample logic.py
@@ -16,16 +16,39 @@ alarmData = {
     'switchDuration': 100,
     'rtcTime': '2:55:2.0',
     'hasTriggered': False,
-    'willrepeat': False
+    'willRepeat': False
 }
 
 
 def process_alarm(alarmData):
     if alarmData['alarmID'] == -1:
         return
+
     currentDatetime = datetime.datetime.now().strftime("%H:%M:%S.%f")
     rtcTime = datetime.datetime.strptime(alarmData['rtcTime'], "%H:%M:%S.%f")
     timeDelta = currentDatetime - rtcTime
+
     if 0 < timeDelta.total_seconds() < timeThreshold:
         print("withinThreshold")
-    
+        if not alarmData['hasTriggered'] or alarmData['willRepeat']:
+            if alarmData['alarmMode'] == 0:
+                print(f'Switching Off: {alarmData["alarmID"]}')
+            elif alarmData['alarmMode'] == 1:
+                print(f'Switching On: {alarmData["alarmID"]}')
+            else:
+                print(f'Transient switching the: {alarmData["alarmID"]}')
+        else:
+            print('Pass 1')
+            pass
+    else:
+        if timeDelta.total_seconds() >= alarmData['switchDuration']:
+            if alarmData['alarmMode'] == 0:
+                print(f'[Inverting] Switching On: {alarmData["alarmID"]}')
+            elif alarmData['alarmMode'] == 1:
+                print(f'[Inverting] Switching Off: {alarmData["alarmID"]}')
+            else:
+                print(f'[Inverting] Transient switching the: {alarmData["alarmID"]}')
+
+        else:
+            print('Pass 2')
+            pass
